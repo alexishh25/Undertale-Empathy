15c480f159f877014fc8e88d8bf1a48f55bc4ee9
Carlos Amílcar Fuentes Lopez <rosalp384@gmail.com>
2025-08-18 11:32:24 -0600

    Movimiento del Personaje

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -10,13 +10,17 @@ public partial class Player : CharacterBody2D
     public float Speed { get; set; } = 400;
     public Vector2 ScreenSize;
     public AnimatedSprite2D AnimatorSprite;
+    public AnimationPlayer AnimationPlayer;
     private string AnimationString = "";
     private Timer timer;
     private Vector2 LastRegisteredDirection = Vector2.Down;
+    public CollisionShape2D colision;
     public override void _Ready()
     {
         ScreenSize = GetViewportRect().Size;
         AnimatorSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        AnimationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        colision = GetNode<CollisionShape2D>("CollisionShape2D");
         timer = GetNode<Timer>("Timer");
     }
     public override void _Process(double delta)
@@ -28,80 +32,117 @@ public partial class Player : CharacterBody2D
     {
         Vector2 input = Input.GetVector("MoveLeft", "MoveRight", "MoveUp", "MoveDown");
         Velocity = input.Normalized() * Speed;
+        if (Input.IsActionPressed("Run"))
+        {
+            Speed = 125f;
+            AnimationPlayer.SpeedScale = 1.25f;
+        }
+        else if(Input.IsActionJustReleased("Run"))
+        {
+            Speed = 100f;
+            AnimationPlayer.SpeedScale = 1f;
+        }
+
         MoveAndSlide();
-        GD.Print("X: " + input.X + "Y: " + input.Y);
+        GD.Print("X: " + input.X + " Y: " + input.Y);
     }
     void AnimationChecker()
     {
+        //Variables (normalmente solo se vuelve a registrar el input
+        //           y alguna cosa que se quiera modificar del objeto)
         Vector2 input = Input.GetVector("MoveLeft", "MoveRight", "MoveUp", "MoveDown");
-        if(input != Vector2.Zero)
+        var rectShape = colision.Shape as RectangleShape2D;
+
+        #region Deteccion de Movimiento
+        if (input != Vector2.Zero)
         {
             LastRegisteredDirection = input.Normalized();
         }
-        if(input == Vector2.Zero)
+        if (input == Vector2.Zero)
         {
-            if(LastRegisteredDirection.Y < 0)
+            if (LastRegisteredDirection.Y < 0)
             {
-                AnimatorSprite.FlipH = false;
+                colision.Position = new Vector2(-1.5f, 16.5f);
+                rectShape.Size = new Vector2(15.0f, 4.25f);
                 AnimationController("IdleUp");
             }
             if (LastRegisteredDirection.Y > 0)
             {
-                AnimatorSprite.FlipH = false;
+                colision.Position = new Vector2(-1.5f, 16.5f);
+                rectShape.Size = new Vector2(15.0f, 4.25f);
                 AnimationController("IdleDown");
             }
             if (LastRegisteredDirection.X < 0)
             {
-                AnimatorSprite.FlipH = false;
+                colision.Position = new Vector2(-1.5f, 16.5f);
+                rectShape.Size = new Vector2(15.0f, 4.25f);
                 AnimationController("IdleLeft");
             }
             if (LastRegisteredDirection.X > 0)
             {
-                AnimatorSprite.FlipH = true;
-                AnimationController("IdleLeft");
+                colision.Position = new Vector2(1.5f, 16.5f);
+                rectShape.Size = new Vector2(15.0f, 4.25f);
+                AnimationController("IdleRight");
             }
         }
-        if(input.X > 0 && input.Y == 0)
+        if (input.X > 0 && input.Y == 0)
         {
-            AnimatorSprite.FlipH = true;
-            AnimationController("WalkLeft");
+            colision.Position = new Vector2(1.5f, 16.5f);
+            rectShape.Size = new Vector2(15.0f, 4.25f);
+            AnimationController("WalkRight");
         }
         if (input.X < 0 && input.Y == 0)
         {
-            AnimatorSprite.FlipH = false;
+            colision.Position = new Vector2(-1.5f, 16.5f);
+            rectShape.Size = new Vector2(15.0f, 4.25f);
             AnimationController("WalkLeft");
         }
         if (input.X == 0 && input.Y < 0)
         {
-            AnimatorSprite.FlipH = false;
+            colision.Position = new Vector2(-1.5f, 16.5f);
+            rectShape.Size = new Vector2(15.0f, 4.25f);
             AnimationController("WalkUp");
         }
         if (input.X == 0 && input.Y > 0)
         {
-            AnimatorSprite.FlipH = false;
+            colision.Position = new Vector2(-1.5f, 16.5f);
+            rectShape.Size = new Vector2(15.0f, 4.25f);
             AnimationController("WalkDown");
         }
         if (input.X > 0 && input.Y < 0)
         {
-            AnimatorSprite.FlipH = false;
             AnimationController("WalkUp");
         }
         if (input.X < 0 && input.Y < 0)
         {
-            AnimatorSprite.FlipH = false;
+            colision.Position = new Vector2(-1.5f, 16.5f);
+            rectShape.Size = new Vector2(15.0f, 4.25f);
             AnimationController("WalkUp");
         }
         if (input.X > 0 && input.Y > 0)
         {
-            AnimatorSprite.FlipH = false;
+            colision.Position = new Vector2(-1.5f, 16.5f);
+            rectShape.Size = new Vector2(15.0f, 4.25f);
             AnimationController("WalkDown");
         }
         if (input.X < 0 && input.Y > 0)
         {
-            AnimatorSprite.FlipH = false;
+            colision.Position = new Vector2(-1.5f, 16.5f);
+            rectShape.Size = new Vector2(15.0f, 4.25f);
             AnimationController("WalkDown");
         }
+        #endregion
+
     }
+    /*Funcionamiento general de la funcion: 
+     *La funcion de AnimationController funciona de
+     *la siguiente forma la funcion va a recibir un string
+     *con el nombre de la animacion (en el motor) y este
+     *va a evaluar si el AnimationString que vendria siendo la animacion
+     *actual que se esta reproduciendo si no son iguales 
+     *la animacion actual va a cambiar a la ingresada en el valor que 
+     *pide la funcion.
+    */
    async void AnimationController(string animation, float time = 0f)
     {
         if(time > 0)
@@ -119,7 +160,7 @@ public partial class Player : CharacterBody2D
             if(AnimationString != animation)
             {
                 AnimationString = animation;
-                AnimatorSprite.Play(AnimationString);
+                AnimationPlayer.Play(AnimationString);
             }
         }
     }
